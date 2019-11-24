0b3f0a50f29349a045ccfecf6eab3eef9180360e
peterorbb <orbitalist@gmail.com>
2019-11-24 19:33:50 +0300

    About, Post, Posts

--- serve.js
+++ serve.js
@@ -1,9 +1,12 @@
+const compress = require('koa-compress');
+const { Z_SYNC_FLUSH: flush } = require('zlib');
 const Koa = require('koa');
 const serve = require('koa-static');
 
 const app = new Koa();
 
 // serve build
+app.use(compress({ filter: contentType => /text/i.test(contentType), threshold: 2048, flush }));
 app.use(serve(`${__dirname}/dist`));
 
 // launch the server
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,46 @@
 import Vue from 'vue';
 import Router from 'vue-router'
 
+import About from '../components/About';
 import Dashboard from '../components/Dashboard';
 import Index from '../components/Index';
 import Login from '../components/Login';
+import Post from '../components/Post';
+import Posts from '../components/Posts';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      component: About,
+      name: 'About',
+      path: '/about',
+    },
+    {
       path: '/dashboard',
       name: 'Dashboard',
       component: Dashboard,
     },
     {
-      path: '/',
-      name: 'Index',
       component: Index,
+      name: 'Index',
+      path: '/',
     },
     {
       path: '/login',
       name: 'Login',
       component: Login,
+    },
+    {
+      path: '/post/:id',
+      name: 'Post',
+      component: Post,
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts,
     },
   ],
 });
